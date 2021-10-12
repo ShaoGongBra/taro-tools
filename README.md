2834ec897c2101ad710c1485131708b594d93ab4
xujian <908634674@qq.com>
2021-10-12 09:33:17 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+# TaroTools
+
+Taro的常用函数封装
---
+++ index.js
@@ -0,0 +1,17 @@
+export * from './lib/net'
+
+export * from './lib/date'
+
+export * from './lib/color'
+
+export * from './lib/object'
+
+export * from './lib/string'
+
+export * from './lib/verify'
+
+export * from './lib/util'
+
+export * from './lib/router'
+
+export * from './lib/event'
